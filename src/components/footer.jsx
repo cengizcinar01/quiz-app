4001d67c6ad119448a56747c7fe3a2e3daccf46c
Cengiz Cinar <c.cinar@mail.de>
2024-04-15 23:37:08 +0200

    Revert "feat: add footer component and styling"
    This reverts commit c5b0bcddb1d4a9f6fb18c5b98e319a11bad300d3.

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,18 +1,9 @@
 import React from 'react';
-import './css/footer.css';
 
 export default function Footer() {
     return (
         <>
-            <footer>
-                <div className="logo-copyright">
-                    <div className="footer-logo"></div>
-                    <span>2024 © All rights reserved.</span>
-                    <li className="footer-impressum">
-                        <a href="#">Impressum</a>
-                    </li>
-                </div>
-            </footer>
+            <div></div>
         </>
     );
 }
