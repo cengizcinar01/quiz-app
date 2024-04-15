3b1cdf16aec0d8cc28b0bf0711ec01f6af3e672d
Cengiz Cinar <c.cinar@mail.de>
2024-04-15 12:57:02 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './components/header';
+import Home from './components/home';
+import Footer from './components/footer';
+
+function App() {
+    return (
+        <>
+            <Header />
+            <Home />
+            <Footer />
+        </>
+    );
+}
+
+export default App;
