d5b193b94c8aaee9b7e717d2fb6861b316a7536e
Cengiz Cinar <c.cinar@mail.de>
2024-04-15 19:35:54 +0200

    feat: update assets and styles for header component

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import './css/header.css';
+import logo from '../assets/devquiz-logo.png';
+import arrowIcon from '../assets/arrow-right.svg';
 
 export default function Header() {
     return (
         <>
-            <div></div>
+            <header>
+                <img className="logo" src={logo} alt="" />
+                <ul className="main-nav">
+                    <li>
+                        <a className="nav-link" href="">
+                            Home
+                        </a>
+                    </li>
+                    <li>
+                        <a className="nav-link" href="">
+                            Was ist DevQuiz?
+                        </a>
+                    </li>
+                    <li>
+                        <a className="nav-link quiz-nav-btn" href="">
+                            Zum Quiz
+                            <div className="arrow-right-icon-container">
+                                <img src={arrowIcon} alt="" />
+                            </div>
+                        </a>
+                    </li>
+                </ul>
+            </header>
         </>
     );
 }
