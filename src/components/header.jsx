9dd883e2b000ba6a7641e2ebcfd569fc6e5e9e1a
Cengiz Cinar <c.cinar@mail.de>
2024-04-18 15:40:26 +0200

    style: reformat code for better readability

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,63 +1,75 @@
-import './css/header.css';
-import logo from '../assets/devquiz-logo.png';
-import arrowIcon from '../assets/arrow-right.svg';
+import "./css/header.css";
+import logo from "../assets/devquiz-logo.png";
+import arrowIcon from "../assets/arrow-right.svg";
 
 export default function Header({ currentPage, changePage, resetQuiz }) {
-    const handleQuizRestart = () => {
-        resetQuiz();
-        changePage('quiz');
-    };
+  const handleQuizRestart = () => {
+    resetQuiz();
+    changePage("quiz");
+  };
 
-    const scrollToInfoSection = () => {
-        const section = document.getElementById('info-section');
-        if (section) {
-            section.scrollIntoView({ behavior: 'smooth' });
-        }
-    };
+  const scrollToInfoSection = () => {
+    const section = document.getElementById("info-section");
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" });
+    }
+  };
 
-    return (
-        <>
-            <header>
-                <img className="logo" src={logo} alt="DevQuiz Logo" />
-                <ul className="main-nav">
-                    {currentPage === 'quiz' || currentPage === 'quizInfo' ? (
-                        <li>
-                            <a className="nav-link hover-text" onClick={() => changePage('home')}>
-                                Zurück zur Startseite
-                            </a>
-                        </li>
-                    ) : currentPage === 'advertising' || currentPage === 'result' ? (
-                        <>
-                            <li>
-                                <a className="nav-link hover-text" onClick={() => changePage('home')}>
-                                    Zurück zur Startseite
-                                </a>
-                            </li>
-                            <li>
-                                <a className="nav-link hover-text" onClick={handleQuizRestart}>
-                                    Quiz wiederholen
-                                </a>
-                            </li>
-                        </>
-                    ) : (
-                        <>
-                            <li>
-                                <a className="nav-link hover-text" onClick={scrollToInfoSection}>
-                                    Was ist DevQuiz?
-                                </a>
-                            </li>
-                            <li>
-                                <a className="nav-link quiz-nav-btn hover-text" onClick={() => changePage('quizInfo')}>
-                                    Zum Quiz
-                                    <div className="arrow-right-icon-container">
-                                        <img src={arrowIcon} alt="" />
-                                    </div>
-                                </a>
-                            </li>
-                        </>
-                    )}
-                </ul>
-            </header>
-        </>
-    );
+  return (
+    <>
+      <header>
+        <img className="logo" src={logo} alt="DevQuiz Logo" />
+        <ul className="main-nav">
+          {currentPage === "quiz" || currentPage === "quizInfo" ? (
+            <li>
+              <a
+                className="nav-link hover-text"
+                onClick={() => changePage("home")}
+              >
+                Zurück zur Startseite
+              </a>
+            </li>
+          ) : currentPage === "advertising" || currentPage === "result" ? (
+            <>
+              <li>
+                <a
+                  className="nav-link hover-text"
+                  onClick={() => changePage("home")}
+                >
+                  Zurück zur Startseite
+                </a>
+              </li>
+              <li>
+                <a className="nav-link hover-text" onClick={handleQuizRestart}>
+                  Quiz wiederholen
+                </a>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <a
+                  className="nav-link hover-text"
+                  onClick={scrollToInfoSection}
+                >
+                  Was ist DevQuiz?
+                </a>
+              </li>
+              <li>
+                <a
+                  className="nav-link quiz-nav-btn hover-text"
+                  onClick={() => changePage("quizInfo")}
+                >
+                  Zum Quiz
+                  <div className="arrow-right-icon-container">
+                    <img src={arrowIcon} alt="" />
+                  </div>
+                </a>
+              </li>
+            </>
+          )}
+        </ul>
+      </header>
+    </>
+  );
 }
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,40 +1,49 @@
-import './css/home.css';
+import "./css/home.css";
 
 export default function Home({ changePage }) {
-    return (
-        <>
-            <main>
-                <div className="hero-section">
-                    <div className="hero-text-section">
-                        <h1 className="h1-text">
-                            Wie gut bist Du <span className="h1-text-hero-span">wirklich?</span>
-                        </h1>
-                        <p className="p-text">Finde heraus, ob Du ein Coding-Profi bist.</p>
-                        <div className="hero-btn-container">
-                            <button className="btn-home-style" onClick={() => changePage('quizInfo')}>
-                                Quiz Starten
-                            </button>
-                        </div>
-                    </div>
-                    <div className="hero-img"></div>
-                </div>
-                <div className="info-section" id="info-section">
-                    <div className="question-img"></div>
-                    <div className="info-text-section">
-                        <h1 className="h1-text">
-                            Was ist <span className="h1-text-hero-span">DevQuiz?</span>
-                        </h1>
-                        <p className="p-text">
-                            Entdecke, teste und verbessere Deine Entwicklerfähigkeiten mit anspruchsvollen Fragen zu verschiedenen Programmiersprachen und Technologien.
-                        </p>
-                        <div className="hero-btn-container">
-                            <button className="btn-home-style" onClick={() => changePage('quizInfo')}>
-                                Starte Dein Quiz
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </main>
-        </>
-    );
+  return (
+    <>
+      <main>
+        <div className="hero-section">
+          <div className="hero-text-section">
+            <h1 className="h1-text">
+              Wie gut bist Du{" "}
+              <span className="h1-text-hero-span">wirklich?</span>
+            </h1>
+            <p className="p-text">Finde heraus, ob Du ein Coding-Profi bist.</p>
+            <div className="hero-btn-container">
+              <button
+                className="btn-home-style"
+                onClick={() => changePage("quizInfo")}
+              >
+                Quiz Starten
+              </button>
+            </div>
+          </div>
+          <div className="hero-img"></div>
+        </div>
+        <div className="info-section" id="info-section">
+          <div className="question-img"></div>
+          <div className="info-text-section">
+            <h1 className="h1-text">
+              Was ist <span className="h1-text-hero-span">DevQuiz?</span>
+            </h1>
+            <p className="p-text">
+              Entdecke, teste und verbessere Deine Entwicklerfähigkeiten mit
+              anspruchsvollen Fragen zu verschiedenen Programmiersprachen und
+              Technologien.
+            </p>
+            <div className="hero-btn-container">
+              <button
+                className="btn-home-style"
+                onClick={() => changePage("quizInfo")}
+              >
+                Starte Dein Quiz
+              </button>
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
 }
